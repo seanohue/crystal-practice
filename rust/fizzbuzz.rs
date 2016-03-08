bb8b6e43884b89bcf1428c96801b581353db64f5
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-03-08 14:22:25 -0500

    Whitespace changes to rust fizzbuzz

--- rust/fizzbuzz.rs
+++ rust/fizzbuzz.rs
@@ -2,18 +2,22 @@ fn main(){
 	let mut count = 0u32;
 	
 	loop{
-
 		count += 1;
+
 		if count % 3 == 0 {
 			print!("Fizz");
 		}
+
 		if count % 5 == 0 {
 			print!("Buzz");
 		}
+
 		if (count % 3 != 00 && count % 5 != 0) {
 			print!("{}", count);
 		}
+
 		println!("");
+		
 		if (count == 100) {
 			break;
 		}
