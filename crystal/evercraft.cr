c14db056e9071a623965fcaee91df7c4e89fff9e
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-04-01 11:55:52 -0400

    Alignment and specs :sunny:

--- crystal/evercraft.cr
+++ crystal/evercraft.cr
@@ -1,9 +1,10 @@
-# As a character I want to have a name so that I can be distinguished from other characters
+
 require "spec"
 
 class Character
   def initialize(name)
     @name = name
+    @alignment = "Neutral"
   end
 
   def name
@@ -13,23 +14,57 @@ class Character
   def name(name)
     @name = name
   end
+
+  def alignment
+    @alignment
+  end
+
+  def alignment(al)
+    if ["Good", "Evil", "Neutral"].includes? al.capitalize
+      @alignment = al.capitalize
+    end
+  end
 end
+
+# spec
 
 describe "Character" do
 
-  describe "#getName" do
+  # As a character I want to have a name so that I can be distinguished from other characters
+
+  describe "#name" do
     it "correctly gets the name" do
       test_char = Character.new "Triss"
       test_char.name.should eq "Triss"
     end
-  end
-
-  describe "#setName" do
+    
     it "correctly sets the name" do
       test_char = Character.new "Bob"
       test_char.name "Frangelica"
       test_char.name.should eq "Frangelica" 
     end
   end
+
+  # As a character I want to have an alignment so that I have something to guide my actions
+
+  describe "#alignment" do
+    it "correctly gets the alignment" do
+      test_char = Character.new "Hodor"
+      test_char.alignment.should eq "Neutral"
+    end
+
+    it "correctly sets the alignment" do
+      test_char = Character.new "Beelzebub"
+      test_char.alignment "evil"
+      test_char.alignment.should eq "Evil"
+    end
+
+    it "only works for valid alignments" do
+      test_char = Character.new "Burrito"
+      test_char.alignment "taco bell"
+      test_char.alignment.should eq "Neutral"
+    end
+  end
+
 
 end
