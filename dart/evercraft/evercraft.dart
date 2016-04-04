513ff12834fb642385eb400411ce5626206988bd
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-04-03 20:54:34 -0400

    Add tests for and alignment stuff to dart evercraft :sunny:

--- dart/evercraft/evercraft.dart
+++ dart/evercraft/evercraft.dart
@@ -2,6 +2,7 @@ import "package:test/test.dart";
 
 class Character {
   String name;
+  String alignment = "Neutral";
 
   Character(this.name);
 }
@@ -18,6 +19,17 @@ void main() {
       var testchar = new Character('Seth');
       testchar.name = 'Jansen';
       expect(testchar.name, equals('Jansen'));
+    });
+
+    test("should have an alignment", () {
+      var testchar = new Character('Frodo');
+      expect(testchar.alignment, equals('Neutral'));
+    });
+
+    test("should be able to change alignment", () {
+      var testchar = new Character('Cinder');
+      testchar.alignment = "Evil";
+      expect(testchar.alignment, equals('Evil'));
     });
 
   });
