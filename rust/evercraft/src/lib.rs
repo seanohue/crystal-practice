cefb71dad03bf43ef9cdc44a65b10dc356b1b412
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-04-06 14:33:49 -0400

    Chars have alignment :sunny: and edit gitignore, go away debug folder.

--- rust/evercraft/src/lib.rs
+++ rust/evercraft/src/lib.rs
@@ -4,7 +4,8 @@
 mod test {
 
     struct Character {
-      name: String
+      name: String,
+      alignment: String
     }
 
     #[test]
@@ -13,14 +14,30 @@ mod test {
     
     #[test]
     fn can_name() {
-      let bob = Character { name: "Bob".to_string() };
+      let bob = Character { 
+        name:      "Bob" .to_string(),
+        alignment: "good".to_string() 
+      };
       assert!(bob.name == "Bob");
     }
 
     #[test]
     fn can_change_name() {
-      let mut sally = Character { name: "Francesca".to_string() };
+      let mut sally = Character { 
+        name:      "Francesca".to_string(),
+        alignment: "good"     .to_string() 
+      };
       sally.name = "Sally".to_string();
       assert!(sally.name == "Sally");
+    }
+
+    #[test]
+    fn has_alignment() {
+      let surprise = Character {
+        name:      "Ciri"   .to_string(), 
+        alignment: "neutral".to_string()
+      };
+
+      assert!(surprise.alignment == "neutral");
     }
 }
