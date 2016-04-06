6323c901eb9f17b70a719b9d994372e8404843f5
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-04-05 22:52:34 -0400

    Safe to say I still don't know rust :droplet:

--- rust/evercraft/src/lib.rs
+++ rust/evercraft/src/lib.rs
@@ -1,6 +1,19 @@
+
+
 #[cfg(test)]
 mod test {
+
+    struct Character {
+      name: String
+    }
+
     #[test]
     fn it_works() {
+    }
+    
+    #[test]
+    fn can_name() {
+      let bob = Character { name: "Bob" };
+      assert!(bob.name == "Bob");
     }
 }
---
+++ rust/evercraft/target/debug/evercraft-fc68344685cfe62d.d
@@ -0,0 +1,5 @@
+/Users/seanodonohue/myProjects/exploration-station/rust/evercraft/target/debug/evercraft-fc68344685cfe62d.d: src/lib.rs
+
+/Users/seanodonohue/myProjects/exploration-station/rust/evercraft/target/debug/evercraft-fc68344685cfe62d: src/lib.rs
+
+src/lib.rs:
