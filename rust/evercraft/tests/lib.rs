2585d57b6c105c9fe32f0fa3bc4c279cbccd97c4
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-04-15 10:28:31 -0400

    Extract tests :wrench:

---
+++ rust/evercraft/tests/lib.rs
@@ -0,0 +1,69 @@
+#[cfg(test)]
+mod test {
+
+    use Character;
+
+    #[test]
+    fn it_works() {}
+
+    #[test]
+    fn has_name() {
+      let bob = Character {
+        name:      "Bob" .to_string(),
+        alignment: "good".to_string(),
+        ..Default::default()
+      };
+
+      assert!(bob.name == "Bob");
+    }
+
+    #[test]
+    fn can_change_name() {
+      let mut sally = Character {
+        name:      "Francesca".to_string(),
+        alignment: "good"     .to_string(),
+        ..Default::default()
+      };
+
+      sally.name = "Sally".to_string();
+
+      assert!(sally.name == "Sally");
+    }
+
+    #[test]
+    fn has_alignment() {
+      let surprise = Character {
+        name:      "Ciri"   .to_string(),
+        ..Default::default()
+      };
+
+      assert!(surprise.alignment == "neutral");
+    }
+
+    #[test]
+    fn can_change_alignment() {
+      let mut ben = Character {
+        name:      "Ben Solo".to_string(),
+        ..Default::default()
+      };
+
+      ben.name      = "Kylo Ren".to_string();
+      ben.alignment = "evil"    .to_string();
+
+      assert!(ben.alignment == "evil");
+    }
+
+    #[test]
+    fn has_default_hp() {
+      let yoda: Character = Default::default();
+
+      assert!(yoda.hitpoints == 5);
+    }
+
+    #[test]
+    fn has_default_ac() {
+      let gorgonzola: Character = Default::default();
+
+      assert!(gorgonzola.armorclass == 10);
+    }
+}
